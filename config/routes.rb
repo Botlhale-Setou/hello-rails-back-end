58927fd80d8808b90109a73d91313bfdfaf00631
Botlhale Setou <botlhale.setou@gmail.com>
2023-05-12 22:34:39 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'root#index'
   namespace :api do
     namespace :v1 do
       resources :greetings, only: [:index]
